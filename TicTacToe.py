def8395b02e424f18720c820bb5c810018f990a0
Felix Prior <felix@firemail.cc>
2019-09-06 16:51:56 -0400

    Resolved issue with ascending diagonal victory and cleaned up

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,41 +1,49 @@
-# player 1 is X
-# player 2 is O
+#Constants
+EMPTY = 0
+PLAYER1 = 1
+PLAYER2 = 2
+SIZE = 3
+
+
 
 def main():
     playGame()
 
 
+
 def playGame():
-    tile = [[None for i in range(3)] for j in range(3)] # create blank game board
+    tile = [[EMPTY for i in range(SIZE)] for j in range(SIZE)]
     end = False
 
     display(tile)
     while not end:
-        turn(1, tile)
+        turn(PLAYER1, tile)
         display(tile)
         end = gameEnd(tile)
         if end != True:
-            turn(2, tile)
+            turn(PLAYER2, tile)
             display(tile)
             end = gameEnd(tile)
         
 
-def display(tile = []):
+
+def display(tile):
     print("    A   B   C ")
-    for y in range (0, 3):
+    for y in range (0, SIZE):
         print(" " + str(y + 1) + " ", end ="")
-        for x in range(0, 3):
-            if tile[x][y] is None:
+        for x in range(0, SIZE):
+            if tile[x][y] == EMPTY:
                 print("   ", end ="")
-            elif tile[x][y] == 1:
+            elif tile[x][y] == PLAYER1:
                 print(" X ", end ="")
-            elif tile[x][y] == 2:
+            elif tile[x][y] == PLAYER2:
                 print(" O ", end ="")
-            if x is not 2:
+            if x != SIZE - 1:
                 print("|", end ="")
-        if y is not 2:
+        if y != SIZE - 1:
             print("\n   ---+---+---")
     print("\n")
+
 
 
 def turn(player, tile):
@@ -45,23 +53,23 @@ def turn(player, tile):
     error = 0
 
     print("Player " + str(player) + "'s turn")
-    print("Please input your target location (formated as A1, B3, etc.): ")
-    input1 = input()
+    print("Formatting: Format your entries as [LETTER][NUMBER], where letter represents X and number represents Y. Example: A1")
+    #print("Please input your target location (formated as A1, B3, etc.): ")
+    input1 = input("Please input your target location: ")
     tilex, tiley = parse(input1)
-    error = check(tilex, tiley, tile)
-    while error is not 0:
-        if error is 1:
-            print("Incorrect input format. Please try again (example of correct formating: C3, B2): ")
-        if error is 2:
-            print("Selected tile is out of range. Please select a different tile: ")
-        elif error is 3: 
-            print("Selected tile is already occupied. Please select a different tile: ")
-        input1 = input()
+    error = errCheck(tilex, tiley, tile)
+    while error != 0:
+        if error == 1:
+            input1 = input("Incorrect input format. Please try again (example of correct formating: C3, B2): ")
+        elif error == 2:
+            input1 = input("Selected tile is out of range. Please select a different tile: ")
+        elif error == 3: 
+            input1 = input("Selected tile is already occupied. Please select a different tile: ")
         tilex, tiley = parse(input1)
-        error = check(tilex, tiley, tile)
+        error = errCheck(tilex, tiley, tile)
     place(tilex, tiley, tile, player)
-
     print("")
+
 
 
 def parse(input1):
@@ -73,9 +81,9 @@ def parse(input1):
     if inplen is 2:
         x = input1[0]
         y = input1[1]
-        if ord(x) - 16 >= 49 and ord(x) - 16 <= 57:
+        if ord(x) - 16 >= 49 and ord(x) - 16 < 49 + SIZE:
             x = int(chr(ord(x) - 17))
-        elif ord(x) - 48 >= 49 and ord(x) - 48 <= 57:
+        elif ord(x) - 48 >= 49 and ord(x) - 48 < 49 + SIZE:
             x = int(chr(ord(x) - 49))
         else:
             x = -1
@@ -86,76 +94,95 @@ def parse(input1):
     else:
         x = -1
         y = -1
+
     return x, y
 
 
 
-def check(tilex, tiley, tile):
+def errCheck(tilex, tiley, tile):
     error = 0
+
     if tilex < 0 or tiley < 0:
         error = 1
-    elif tilex > 2 or tiley > 2:
+    elif tilex > SIZE - 1 or tiley > SIZE - 1:
         error = 2
-    elif tile[tilex][tiley] is not None:
+    elif tile[tilex][tiley] != EMPTY:
         error = 3
     
     return error
 
+
+
 def place(tilex, tiley, tile, player):
-    print(tilex)
-    print(tiley)
     tile[tilex][tiley] = player
-    print(tile)
+
+
 
 def gameEnd(tile):
     end = False
     winner = checkWinner(tile)
 
-    if winner != 0:
-        print("Player " + str(winner) + " wins!")
+    if winner == PLAYER1:
+        print("Player 1 wins!")
+        end = True
+    elif winner == PLAYER2:
+        print("Player 2 wins!")
         end = True
 
     return end
 
-    
+
 
 def checkWinner(tile):
     winner = 0
-    for y in range(0,3):
-        for x in range(0, 3):
+
+    for y in range(0, SIZE):
+        for x in range(0, SIZE):
             player = tile[x][y]
-            if player != None:
-                if checkHor(x, y, player, tile):
+            if player != EMPTY:
+                if checkHor(x, y, player, tile) == 2:
                     winner = player
-                elif checkVert(x, y, player, tile):
+                elif checkVert(x, y, player, tile) == 2:
                     winner = player
-                elif checkDiag(x, y, player, tile):
+                elif checkDiagDown(x, y, player, tile) == 2:
+                    winner = player
+                elif checkDiagUp(x, y, player, tile) == 2:
                     winner = player
                 
     return winner
 
 
 
-
 def checkHor(x, y, player, tile):
-    matches = 1
-    match3 = checkTile(x, y, 1, 0, player, matches, tile)
+    matches = 0
+    matches = checkTile(x, y, 1, 0, player, matches, tile)
 
-    return match3
+    return matches
+
 
 
 def checkVert(x, y, player, tile):
-    matches = 1
-    match3 = checkTile(x, y, 0, 1, player, matches, tile)
+    matches = 0
+    matches = checkTile(x, y, 0, 1, player, matches, tile)
 
-    return match3
+    return matches
 
 
-def checkDiag(x, y, player, tile):
-    matches = 1
-    match3 = checkTile(x, y, 1, 1, player, matches, tile)
 
-    return match3
+def checkDiagDown(x, y, player, tile):
+    matches = 0
+    matches = checkTile(x, y, 1, 1, player, matches, tile)
+
+    return matches
+
+
+
+def checkDiagUp(x, y, player, tile):
+    matches = 0
+    matches = checkTile(x, y, 1, -1, player, matches, tile)
+
+    return matches
+
 
 
 def checkTile(x, y, xdir, ydir, player, matches, tile):
@@ -165,15 +192,13 @@ def checkTile(x, y, xdir, ydir, player, matches, tile):
     #print("x:" + str(x))
     #print("y: " + str(y))
     #print("Matches: " + str(matches))
-    if x < 3 and y < 3 and matches < 3:
+    if x < SIZE and y < SIZE and x >= 0 and y >= 0:
         if tile[x][y] == player:
             matches += 1
-            match3 = checkTile(x, y, xdir, ydir, player, matches, tile)
-    elif matches == 3:
-        match3 = True
+            matches = checkTile(x, y, xdir, ydir, player, matches, tile)
 
-    return match3
-            
+    return matches
+
 
 
 main()
